923b5fe5fe874dc6928090c041c972862e87729f
nin <nin@ciklum.com>
2019-11-18 00:28:05 +0300

    Working stack

--- cdk_app/lib/cdk_app-stack.ts
+++ cdk_app/lib/cdk_app-stack.ts
@@ -1,6 +1,7 @@
 import cdk = require('@aws-cdk/core');
 import ec2 = require('@aws-cdk/aws-ec2');
 import ecs = require('@aws-cdk/aws-ecs');
+import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
 
 export class CdkAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -14,6 +15,25 @@ export class CdkAppStack extends cdk.Stack {
       streamPrefix: "jupiter_lab",
     })
 
+    // === Load balancer stack ===
+    // // Instantiate Fargate Service with just cluster and image
+    // const fargateService = new ecs_patterns.NetworkLoadBalancedFargateService(this, "JupiterLabLbFargateService", {
+    //   cluster,
+    //   taskImageOptions: {
+    //     image: ecs.ContainerImage.fromRegistry("jupyter/datascience-notebook"),
+    //     containerPort: 8888,
+    //     containerName: 'JupiterLabNotebook',
+    //   },
+    //   assignPublicIp: true,
+    //   memoryLimitMiB: 512,
+    //   cpu: 256,
+    // });
+
+    // // Output the DNS where you can access your service
+    // new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: fargateService.loadBalancer.loadBalancerDnsName });
+
+
+    // === Normal stack ===
     const taskDef = new ecs.FargateTaskDefinition(this, "JupiterLabTaskDefinition", {
       memoryLimitMiB: 512,
       cpu: 256,
